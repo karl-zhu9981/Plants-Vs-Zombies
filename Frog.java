2276088f74422e6e879b8ea300fec96acc3acfb9
Karl Zhu <caivin2009@hotmail.com>
2018-02-19 21:36:37 -0500

    Update Frog.java

--- Frog.java
+++ Frog.java
@@ -1,10 +1,17 @@
+//Karl Zhu and Ibraheem Aloran
+//ICS 4U
+//Frog.java
+//This class makes our frog object and the information that it goes with, the controllable character by the users
+//They contain the position of the frog, the width and height of the frog and the number of lives that frog has.
 import java.util.*;
 import java.awt.*;
 public class Frog{
-    private int x, y, width, height, v, lives;
-    private String pic;
+    private int x, y, width, height, v, lives;//The information for the frog
+    private String pic;//The picture of the frog character
 
-    public Frog(){
+    public Frog(){//Constructor for our frog
+    //Initial stats for the frog
+    //Starting location, size, pic, speed, and number of lives
     	x = 400;
     	y = 560;
     	width = 25;
@@ -14,61 +21,61 @@ public class Frog{
     	lives = 3;
 
     }
-    public void jump(int direction){
+    public void jump(int direction){//Allows the frog to move based on the direction it wants to go
     	//left = 1, up = 2, right = 3, down = 4
-
+		//Movement by the speed of each action in the desired direction
     	if(direction == 1){
-    		if (x-v>=0){
+    		if (x-v>=0){//Makes sure frog is within the boundaries
     			x -= v;
     		}
     	}
     	if(direction == 2){
-    		if (y-v>=0){
+    		if (y-v>=0){//Makes sure frog is within the boundaries
     			y -= v;
     		}
     	}
     	if(direction == 3){
-    		if (x+v<=800){
+    		if (x+v<=800){//Makes sure frog is within the boundaries
     			x += v;
     		}
     	}
     	if(direction == 4){
-    		if (y+v<=600){
+    		if (y+v<=600){//Makes sure frog is within the boundaries
     			y+= v;
     		}
     	}
     }
-    public String pik(){
+    public String pik(){//Returns the picture of the frog
     	return pic;
     }
-    public int gitx(){
+    public int gitx(){//Gets the x coordinate of the frog
     	return x;
     }
-    public int gitx(int n){
+    public int gitx(int n){//Increases the x coordinate by n pixels and gets the x coordinate of the frog
     	x += n;
     	return x;
     }
-    public int gity(){
+    public int gity(){//Gets the y coordinate of the frog
     	return y;
     }
-    public int getw(){
+    public int getw(){//Gets the width of the frog
     	return width;
     }
-    public int geth(){
+    public int geth(){//Gets the height of the frog
     	return height;
     }
-    public int life(){
+    public int life(){//Gets the number of lives for the frog
     	return lives;
     }
-    public int life(int x){
+    public int life(int x){//Decreases the number of lives by 1 and gets the number of lives for the frog
     	lives -= 1;
     	return lives;
     }
-    public void reset(){
+    public void reset(){//When the frog dies, and we need to reset the frog back to its initial position
     	x = 400;
     	y = 560;
     }
-	public Rectangle getR(){
+	public Rectangle getR(){//Gets the rectangle of the frog onject
 		return new Rectangle(x+3,y+3,width, height);
 	}
 
