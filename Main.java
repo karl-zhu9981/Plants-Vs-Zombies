7b51b4c1069888c57f8f7ce249dd79cc5e76e834
Karl Zhu <caivin2009@hotmail.com>
2018-05-01 10:56:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,23 +3,27 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.image.*;
-public class Main extends JFrame implements ActionListener{//Our main class. Checks for actions and keys
+public class Main extends JFrame implements MouseListener, ActionListener{//Our main class. Checks for actions and keys
 	private Image menuPic;//Our menu picture
+	JPanel cards;   	//a panel that uses CardLayout
+    CardLayout cLayout = new CardLayout();
+	Timer myTimer;
+	GamePanel game;
     public Main(){
     	super("Plants Vs Zombies");//Our caption and using the super class
     	game = new GamePanel();
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	setSize(800,600);//Size of our screen
+    	setSize(1200,750);//Size of our screen
 		myTimer = new Timer(10,this);
 		myTimer.start();
 
-		menuPic = new ImageIcon("images.jpg").getImage();//Background image
-		menuPic = menuPic.getScaledInstance(800,600,Image.SCALE_SMOOTH);
+		menuPic = new ImageIcon("menuPic.jpg").getImage();//Background image
+		menuPic = menuPic.getScaledInstance(1200,750,Image.SCALE_SMOOTH);
 		JLabel backLabel = new JLabel(new ImageIcon(menuPic));
 		JLayeredPane mPage=new JLayeredPane(); 	// LayeredPane allows my to control what shows on top
 		mPage.setLayout(null);
 
-		backLabel.setSize(800,600);
+		backLabel.setSize(1200,750);
 		backLabel.setLocation(0,0);
 		mPage.add(backLabel,1);					// The numbers I use when adding to the LayeredPane
 												// are just relative to one another. Higher numbers on top.
@@ -28,12 +32,13 @@ public class Main extends JFrame implements ActionListener{//Our main class. Che
 		cards.add(game, "game");
 		add(cards);
 
-    	addKeyListener(this);
-    	setResizable(false);
+    	addMouseListener(this);
+    	//add(game);
+    	setResizable(true);
     	setVisible(true);
     }
     public static void main(String[] args) {
-		new Frogger();//Call the game to run
+		new Main();//Call the game to run
 
 
     }
@@ -41,25 +46,40 @@ public class Main extends JFrame implements ActionListener{//Our main class. Che
     	Object source =evt.getSource();
 		if(source==myTimer){
 			//Runs the main methods to operate our game
-			game.move();
-    		game.traffic();
-    		game.gameState();
    			game.repaint();
     	}
-    	if(game.state()==1){
+    	/*if(game.state()==1){
     		cLayout.show(cards,"menu");
 		    myTimer.start();
 		    requestFocus();//Gets the focus of the program to our game
-    	}
+    	}*/
     }
+    @Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e){}
+
+	@Override
+	public void mousePressed(MouseEvent e){
+		 cLayout.show(cards,"game");
+		 myTimer.start();
+		 requestFocus();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e){}
+
+	@Override
+	public void mouseExited(MouseEvent e){}
 }
 
 class GamePanel extends JPanel implements MouseListener{
 	private Image map, mapPic;
 	public GamePanel(){
 		map= new ImageIcon("Background1.jpg").getImage();
-		mapPic = map.getScaledInstance(800,600,Image.SCALE_SMOOTH);
-		setSize(800,600);
+		mapPic = map.getScaledInstance(1200,750,Image.SCALE_SMOOTH);
+		//setSize(800,600);
 		addMouseListener(this);
 	}
 	@Override
@@ -80,6 +100,7 @@ class GamePanel extends JPanel implements MouseListener{
 
 	@Override
 	public void mouseExited(MouseEvent e){}
+
 	public void paintComponent(Graphics g){
 		g.drawImage(mapPic,0,0,this);
 	}
